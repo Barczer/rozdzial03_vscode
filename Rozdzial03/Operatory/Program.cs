e20ae0e701d7dcfcfeb1581a4f7a9da60a0ef58d
Barczer <152106234+Barczer@users.noreply.github.com>
2025-09-22 13:29:16 +0000

    commit

---
+++ Rozdzial03/Operatory/Program.cs
@@ -0,0 +1,27 @@
+﻿#region Badanie jednoargumentowych
+// int a = 3;
+// int b = a++;
+// WriteLine($"a ma wartość {a}, b ma wartość {b}");
+// int c = 3;
+// int d = ++c;
+// WriteLine($"c ma wartość {c}, d ma wartość {d}");
+// int e = 11;
+// int f = 3;
+// WriteLine($"e + f = {e + f}");
+// WriteLine($"e - f = {e - f}");
+// WriteLine($"e * f = {e * f}");
+// WriteLine($"e / f = {e / f}");
+// WriteLine($"e % f = {e % f}");
+// double g = 11.0;
+// WriteLine($"g ma wartość {g:N1}, f ma wartość {f}");
+// WriteLine($"g / f = {g / f}");
+// string? nazwiskoAutora = ReadLine();
+// int maksDlugosc = nazwiskoAutora?.Length ?? 30;
+// nazwiskoAutora ??= "nieznany";
+
+bool a = true;
+bool b = false;
+
+WriteLine($"a | {a ^ b, 50}");
+
+#endregion
